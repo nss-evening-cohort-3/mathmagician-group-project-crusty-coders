2b20383b4f1a699653a66500e56f2673b8230bc2
Sandy Jones <sandynashv@gmail.com>
2016-08-02 11:12:44 -0500

    eventests.cs not showing in test explorer

---
+++ Mathmagician.Tests/Even.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    internal class  Even
+    {
+    }
+}
---
+++ Mathmagician.Tests/EvenTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    public class EvenTests
+    {
+        [TestMethod]
+        public void EvenEnsureICanCreateAnInstanceOf()
+        {
+            Even my_even = new Even();
+
+            Assert.IsNotNull(my_even);
+        }
+    }
+}
+
