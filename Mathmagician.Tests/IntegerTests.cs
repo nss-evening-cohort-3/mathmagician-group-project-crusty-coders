db70706cf697a4cc20f97bceb4eed88b6e5cf8a6
Sandy Jones <sandynashv@gmail.com>
2016-08-05 18:26:55 -0500

    SubtractOne method and ModuloTwo method added

--- Mathmagician.Tests/IntegerTests.cs
+++ Mathmagician.Tests/IntegerTests.cs
@@ -32,12 +32,26 @@ namespace Mathmagician.Tests
             int answerMulty = Multiply.Multiply(6, 7);
             Assert.AreEqual( (42), answerMulty );
         }
+        [TestMethod]
         public void IntegerAddTwo()
         {
             Integer AddTwo = new Integer();
             int answerAddTwo = AddTwo.AddTwo(3);
             Assert.AreEqual((5), answerAddTwo );
         }
-
+        [TestMethod]
+        public void IntegerSubtractOne()
+        {
+            Integer SubtractOne = new Integer();
+            int answerSO = SubtractOne.SubtractOne(5);
+            Assert.AreEqual((4), answerSO);            
+        }
+        [TestMethod]
+        public void IntegerModuloTwo()
+        {
+            Integer ModuloTwo = new Integer();
+            int answerModTwo = ModuloTwo.ModuloTwo(10);
+            Assert.AreEqual((0), answerModTwo);
+        }
     }
 }
