281fbc4e3030bf5973ece40f5da84bf9ca09b429
GAylabea <gbea@rocketmail.com>
2016-08-11 20:02:41 -0500

    refactored program.cs

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -17,20 +17,72 @@ namespace Mathmagician
             String user_numberType = Console.ReadLine().ToLower();
             Console.WriteLine("How long would you like your list?");
             Console.Write(prompt);
-            String user_listLength = Console.ReadLine();
-            String result = "OK, I am going to print out {user_numberType}{user_numberChoice}";
-            String mistake = "Nope - that is not an acceptable response!";
-
-            //Loops to identify which user_numberType is selected  
-                //Loop to make sure they entered a useable list length? no negative, no decimal?
-                //Console.write new list    
-            if (user_numberType == "integer")
+            String switchToInt = Console.ReadLine();
+            bool intNow = false;
+            int listLength = 0;
+            //this is switching our string-user-response to an integer
+            //parse user listLength
+            intNow = (Int32.TryParse(switchToInt, out listLength));
+            if (listLength > 0)
             {
-                //instantiate Integer
-                Integer integer = new Integer();
-                //generate Integer list
+                //Loops to identify which user_numberType is selected  
+                //Loop to make sure they entered a useable list length? no negative, no decimal?
+                //Console.write new list 
+                Console.WriteLine("OK, I am going to print out " + listLength + " " + user_numberType + "s ");
+                Console.WriteLine("Here are your numbers! Whoopee!");
+                if (user_numberType == "integer")
+                {
+                    //instantiate Integer
+                    Integer myInteger = new Integer();
+                    //we are creating a list for a new integer and applying the method(GenerateList) to create a list of integers
+                    List<int> intList = myInteger.GenerateList(listLength);
+                    //this is calling the method in the Integer.cs class to print the intList to screen
+                    myInteger.ShowNumberListInConsole(intList);
 
+                }
+                else if (user_numberType == "even")
+                {
+
+                    //Integer myEven = new Integer();
+                    //List<int> intList = myEven.GenerateList(listLength);
+                    //myEven.ShowNumberListInConsole(intList);
+                    Console.WriteLine("You picked evens!");
+                }
+                else if (user_numberType == "odd")
+                {
+
+                    //Integer myEven = new Integer();
+                    //List<int> intList = myEven.GenerateList(listLength);
+                    //myEven.ShowNumberListInConsole(intList);
+                    Console.WriteLine("You picked odds!");
+                }
+                else if (user_numberType == "prime")
+                {
+
+                    //Integer myEven = new Integer();
+                    //List<int> intList = myEven.GenerateList(listLength);
+                    //myEven.ShowNumberListInConsole(intList);
+                    Console.WriteLine("You picked primes!");
+                }
+                else if (user_numberType == "fibonacci")
+                {
+
+                    //Integer myEven = new Integer();
+                    //List<int> intList = myEven.GenerateList(listLength);
+                    //myEven.ShowNumberListInConsole(intList);
+                    Console.WriteLine("You picked fibonaccis!");
+                }
             }
+            else
+            {
+                Console.WriteLine("only whole numbers please - no negatives");
+                return; //this ends the program
             }
+            Console.ReadLine();//ends the list length value check if loop
+        }
     }
 }
+            
+                    
+                    
+
